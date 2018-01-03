a37d1a55d1a790302b2b4b6eab0c277a8f3c233c
prasannaprabhu87 <prasannaprabhuhegde87>
2018-01-03 15:21:38 +0530

    1.3

---
+++ 1.3.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+Firstname=input("Enter your first name \n")
+Lastname=input("Enter your last name \n")
+firstnamereverse=Firstname[::-1]
+lastnamereverse=Lastname[::-1]
+print (firstnamereverse +' ' + lastnamereverse)
+
